d6490a13f892afb91f83f1b4361080016ef19dc7
Dick Henrik Larsson <dicklarsson@gmail.com>
2021-07-07 12:20:38 +0200

    Adding authentication

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,13 +5,77 @@ import { Home } from "./Home";
 import { Pay } from "./Pay";
 import { Button, Card } from "ui-neumorphism";
 import firebase from "firebase";
-import  firebaseConfig  from "./firebaseConfig.json";
- 
+import firebaseConfig from "./firebaseConfig.json";
+export type User = firebase.User;
+import { default as StyledFirebaseAuth } from "react-firebaseui/StyledFirebaseAuth";
 const app = firebase.initializeApp(firebaseConfig);
 import "ui-neumorphism/dist/index.css";
 const database = app.database();
 
-function App() {
+const logOut = () => {
+  if (confirm("Do you want to log out?")) {
+    firebase.auth().signOut();
+  }
+};
+
+function Cosmos() {
+  const user = useUser();
+
+  const uiConfig = {
+    signInFlow: "popup",
+    callbacks: {
+      /* signInSuccessWithAuthResult: (authResult, _) => {
+        // setUser(authResult.user);
+        return true;
+      },*/
+      uiShown: function () {},
+    },
+    signInOptions: [
+      // List of OAuth providers supported.
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    ],
+  };
+  if (user) {
+    return <App user={user} logOut={logOut} />;
+  } else {
+    return (
+      <div style={{ marginTop: "50px", padding: "44px" }}>
+        <h1>RVN</h1>
+        <h5>Ravencoin Private Wallet</h5>
+        <StyledFirebaseAuth
+          uiConfig={uiConfig}
+          firebaseAuth={firebase.auth()}
+        />
+      </div>
+    );
+  }
+}
+function useUser() {
+  const [user, setUser] = React.useState(null);
+
+  React.useEffect(() => {
+    firebase.auth().onAuthStateChanged(async function (user: User) {
+      //Check that the user has a user profile
+
+      setUser(user);
+
+      if (user === null) {
+        return;
+      }
+      /*  const data =
+        user.providerData &&
+        user.providerData.length > 0 &&
+        user.providerData[0];
+
+      firebase
+        .database()
+        .ref("users/" + user.uid + "/profile_data")
+        .update(data);*/
+    });
+  }, []);
+  return user;
+}
+function App({user}) {
   const [route, setRoute] = React.useState(Routes.OVERVIEW);
 
   const [assets, setAssets] = React.useState([]);
@@ -49,17 +113,14 @@ function App() {
   };
 
   return (
-    <Card >
+    <Card flat>
+      {user.displayName} - {user.email}
       <ul className="raven-rebels-ego-wallet__nav">
         <li className="raven-rebels-ego-wallet__nav-item">
-          <Button onClick={() => setRoute(Routes.OVERVIEW)}>
-            Home
-          </Button>
+          <Button onClick={() => setRoute(Routes.OVERVIEW)}>Home</Button>
         </li>
         <li className="raven-rebels-ego-wallet__nav-item">
-          <Button onClick={() => setRoute(Routes.PAY)}>
-            Pay
-          </Button>
+          <Button onClick={() => setRoute(Routes.PAY)}>Pay</Button>
         </li>
       </ul>
       {route === Routes.OVERVIEW && (
@@ -74,4 +135,4 @@ function App() {
   );
 }
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<Cosmos />, document.getElementById("app"));
